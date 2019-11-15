ac3a25fefaa6afb7c992cd1299b61f3a1b924d98
Becca Robinson <becca@puppet.com>
2019-11-15 10:01:50 -0600

    Adding inital draft of plan but still more work to do.

--- plans/init.pp
+++ plans/init.pp
@@ -1,12 +1,12 @@
 plan preupgrade_check(
-  TargetSpec $servers,
+  TargetSpec $nodes,
   Boolean    $fail_plan_on_errors = true,
 ) {
-  apply_prep([$servers])
-  
-  if $servers.empty { return ResultSet.new([]) }
+  if $nodes.empty { return ResultSet.new([]) }
 
-  $os_results = run_task(preupgrade_check::check_os, '_catch_errors' => true ) 
+  apply_prep([$nodes])
+
+  $os_results = run_task(preupgrade_check::check_os, $nodes, '_catch_errors' => true ) 
   
   $failed_results = $os_results.filter |$os_result| {
     if !$os_result.ok() {
@@ -29,19 +29,19 @@ plan preupgrade_check(
   }
 
   $codedir_failures = $os_results.filter | $result | {
-    if $result['CodeDirectoryUsage'].value > '50' {
+    if $result['CodeDirectoryUsage'] > '50' {
       true
     }
   }
 
   $puppetdir_failures = $os_results.filter | $result | {
-    if $result['PuppetDirectoryUsage'].value > '50' {
+    if $result['PuppetDirectoryUsage'] > '50' {
       true
     }
   }
 
 
-  $time_results = run_task(preupgrade_check::check_time, '_catch_errors' => true )
+  $time_results = run_task(preupgrade_check::check_time, $nodes, '_catch_errors' => true )
   
   
 
